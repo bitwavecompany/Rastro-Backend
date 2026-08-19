ff57a1e562f5c10829bc1756db57f7210644bac9
cristian-diaz <dannycriss12@gmail.com>
2026-08-18 21:32:50 -0500

    Fuerza la actualizacion del controlador y limpia cache

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
  <?php
 
- use Illuminate\Http\Request;
- use Illuminate\Support\Facades\Route;
- use App\Http\Controllers\ExpenseController;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\ExpenseController;
 
- Route::get('/gastos', [ExpenseController::class, 'index']);
- Route::post('/gastos', [ExpenseController::class, 'store']);
+    Route::get('/ping', function () {
+        return response()->json([
+            'estado' => '¡Render por fin se actualizó!',
+            'datos_de_prueba' => \App\Models\Expense::all()
+        ]);
+    });
+    Route::post('/gastos', [ExpenseController::class, 'store']);
