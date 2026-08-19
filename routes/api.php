f3226a6d85b95c3758aa933ea2d74d3030f672a4
cristian-diaz <dannycriss12@gmail.com>
2026-08-18 22:20:00 -0500

    correccion get api

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,16 @@
- <?php
+<?php
 
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Route;
-    use App\Http\Controllers\ExpenseController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExpenseController;
 
-    Route::get('/ping', function () {
-        return response()->json([
-            'estado' => '¡Render por fin se actualizó!',
-            'datos_de_prueba' => \App\Models\Expense::all()
-        ]);
-    });
-    Route::post('/gastos', [ExpenseController::class, 'store']);
+Route::get('/gastos', [ExpenseController::class, 'index']);
+
+Route::post('/gastos', [ExpenseController::class, 'store']);
+
+Route::get('/ping', function () {
+    return response()->json([
+        'estado' => '¡Render por fin se actualizó!',
+        'datos' => \App\Models\Expense::all()
+    ]);
+});
